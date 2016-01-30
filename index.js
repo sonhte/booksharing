7d1f690432c98280eea2b7cb8c15dce7c65793af
Ryan Mast <rmast@server.fake>
2016-01-30 15:15:26 -0800

    some structure

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var express = require('express');
+var path = require('path');
+
+// booksharing routes
+// var index = require('./routes/index');
+// var groups = require('./routes/groups');
+
 var app = express();
+
+// setup templating engine
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'mustache');
+
+// middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// booksharing route handlers
+// app.use('/', index);
+// app.use('/groups', groups);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');

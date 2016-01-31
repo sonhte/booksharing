51b3e465e675fe8ddcba1704ae4d638e315c20b1
Ryan Mast <rmast@server.fake>
2016-01-30 16:36:30 -0800

    using handlebars template to show index

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var path = require('path');
+var exphbs = require('express-handlebars');
 
 // booksharing routes
-// var index = require('./routes/index');
+var index = require('./routes/index');
 // var groups = require('./routes/groups');
 
 var app = express();
 
 // setup templating engine
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 // middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // booksharing route handlers
-// app.use('/', index);
+app.use('/', index);
 // app.use('/groups', groups);
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
